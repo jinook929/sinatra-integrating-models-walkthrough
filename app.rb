23a1cb0619648b1641fa9e38ef31ef43dceb7311
jinook929 <jinook929@gmail.com>
2021-02-22 17:34:31 -0500

    commit tree edb3067de3c41f58f7364b3fa37be65bc0a0f668

--- app.rb
+++ app.rb
@@ -9,6 +9,13 @@ class App < Sinatra::Base
   post '/' do
     text_from_user = params[:user_text]
 
+    @analyzed_text = TextAnalyzer.new(text_from_user)
+    # @num_of_words = text_from_user.split.count
+    # @vowels = text_from_user.count("AEIOUaeiou")
+    # @consonants = text_from_user.count("^a^e^i^o^u^A^E^I^O^U^ \\w")
+    # @most_common_letter = ""
+    # @most_common_letter_count = nil
+    # binding.pry
     erb :results
   end
 end
